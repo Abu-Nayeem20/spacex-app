6660224fa7d0886bfe9f96bf5d16fbc12f5bb3cd
Abu-Nayeem20 <abu64673@gmail.com>
2022-05-10 05:24:18 +0600

    update

--- src/Components/SpaceLaunch/SingleSpaceLaunch.js
+++ src/Components/SpaceLaunch/SingleSpaceLaunch.js
@@ -2,16 +2,31 @@ import React from 'react';
 
 const SingleSpaceLaunch = ({ launch }) => {
     // console.log("Launch", launch)
-    const { details, flight_number, launch_success, launch_year, mission_name,  } = launch;
+    const { details, flight_number, launch_success, launch_year, mission_name, upcoming, launch_date_local  } = launch;
+
+    const dateSlice = launch_date_local.slice(0,10);
+
+    console.log(dateSlice)
 
     const { rocket_name, rocket_type, rocket_id} = launch.rocket;
     return (
-        <div class="col">
-        <div class="card h-100">
-        <img src="..." class="card-img-top" alt="..."/>
-        <div class="card-body">
-            <h5 class="card-title">{flight_number}</h5>
+        <div className="col">
+        <div className="card h-100">
+        <img src="..." className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">{flight_number}</h5>
             <h5>Rocket Name: {rocket_name}</h5>
+            <h6>Launch Status: {launch_success === true?
+            `Success`
+            :
+            `Failure`
+        }</h6>
+            <h6>Condition: {upcoming === true?
+            `Upcoming`
+            :
+            `Not`
+        }</h6>
+        <h6>Launching Year: {launch_year}</h6>
             <p>{details ?
                  details
                  :
--- src/Components/SpaceLaunch/SpaceLaunch.js
+++ src/Components/SpaceLaunch/SpaceLaunch.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { FormControl, InputGroup, Spinner } from 'react-bootstrap';
+import { FormControl, InputGroup, Modal, Spinner } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchLaunches } from '../../Redux/Slices/spaceSlice';
 import SingleSpaceLaunch from './SingleSpaceLaunch';
 
 const SpaceLaunch = () => {
+
+    const [smShow, setSmShow] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -14,17 +16,90 @@ const SpaceLaunch = () => {
   
     const launches = useSelector((state) => state.launches.launches)
   
-    // console.log("Success", launches)
-    const [searchItems, setSearchItems] = useState(launches);
+    // console.log("Success", launches.upcoming)
+    const [displayItems, setDisplayItems] = useState(launches);
 
     const handleSearch = e => {
         const searchText = e.target.value;
-        console.log(searchText);
+        // console.log(searchText);
         const matchedItems = launches.filter(item => item?.rocket.rocket_name.toLowerCase().includes(searchText.toLowerCase()));
 
-        setSearchItems(matchedItems);
+        setDisplayItems(matchedItems);
     }
-    console.log("init", searchItems)
+    // console.log("init", displayItems)
+
+    // filter data
+
+    const filterUpcoming = e => {
+        const checkedItems = e.target.checked;
+        if (checkedItems === true) {
+            const upComing = launches.filter(item => item.upcoming === true);   
+            setDisplayItems(upComing);
+        }else{
+            setDisplayItems(launches);
+        }
+    }
+
+    const filterSuccess = e => {
+        const checkedItems = e.target.checked;
+        if(checkedItems === true){
+            const success = launches.filter(item => item.launch_success === true);   
+            setDisplayItems(success);
+            
+        }else{
+            setDisplayItems(launches);
+        }
+    }
+    const filterFailure = e => {
+        const checkedItems = e.target.checked;
+        if(checkedItems === true){
+            const failure = launches.filter(item => item.launch_success === false);   
+            setDisplayItems(failure);
+            
+        }else{
+            setDisplayItems(launches);
+        }
+    }
+    const filterLastYear = e => {
+        const checkedItems = e.target.checked;
+        if(checkedItems === true){
+            const lastYear = launches.filter(item => item.launch_year === "2021");
+            if (lastYear.length === 0) {
+                setSmShow(true);
+            }   
+            setDisplayItems(lastYear);
+            
+        }else{
+            setDisplayItems(launches);
+        }
+    }
+    const filterLastMonth = e => {
+        const checkedItems = e.target.checked;
+        if(checkedItems === true){
+            const lastMonth = launches.filter(item => item.launch_date_local.slice(0,7) === "2022-04");
+            if (lastMonth.length === 0) {
+                setSmShow(true);
+            }   
+            setDisplayItems(lastMonth);
+            
+        }else{
+            setDisplayItems(launches);
+        }
+    }
+    const filterLastWeek = e => {
+        const checkedItems = e.target.checked;
+        if(checkedItems === true){
+            const lastWeek = launches.filter(item => item.launch_date_local.slice(0,10) >= "2022-05-03");
+            if (lastWeek.length === 0) {
+                setSmShow(true);
+            }   
+            setDisplayItems(lastWeek);
+            
+        }else{
+            setDisplayItems(launches);
+        }
+    }
+
     return (
         <div className='container-fluid'>
             {/* Search Bar */}
@@ -42,20 +117,76 @@ const SpaceLaunch = () => {
                     <div className="row">
                         <div className="col-md-3">
                             <div className='border-end border-info'>
-                                <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit incidunt blanditiis deleniti esse nobis distinctio laudantium ipsam eveniet rerum repellendus. Porro aliquam et laboriosam aut temporibus asperiores accusamus perferendis quam?</h2>
+                                <div className='text-center'>
+                                    <h4>Data Filter</h4>
+                                </div>
+                                {/* Launch Date  */}
+                                <div>
+                                    <h5 className='fw-bold'>Launch Date</h5>
+                                <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault1"
+                                onChange={filterLastWeek}/>
+                                <label className="form-check-label fw-bold" htmlFor="flexCheckDefault1">
+                                    Last Week
+                                </label>
+                                </div>
+                                <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault2"
+                                onChange={filterLastMonth}/>
+                                <label className="form-check-label fw-bold" htmlFor="flexCheckDefault2">
+                                    Last Month
+                                </label>
+                                </div>
+                                <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault3"
+                                onChange={filterLastYear}/>
+                                <label className="form-check-label fw-bold" htmlFor="flexCheckDefault3">
+                                    Last Year
+                                </label>
+                                </div>
+                                </div>
+                                {/* Launch Status */}
+                                <div>
+                                    <h5 className='fw-bold'>Launch Status</h5>
+                                <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault4"
+                                onChange={filterSuccess}/>
+                                <label className="form-check-label fw-bold" htmlFor="flexCheckDefault4">
+                                    Success
+                                </label>
+                                </div>
+                                <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault5"
+                                onChange={filterFailure}/>
+                                <label className="form-check-label fw-bold" htmlFor="flexCheckDefault5">
+                                    Failure
+                                </label>
+                                </div>
+                                </div>
+                                {/* Condition */}
+                                <div>
+                                    <h5 className='fw-bold'>Condition</h5>
+                                <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault6"
+                                onChange={filterUpcoming}/>
+                                <label className="form-check-label fw-bold" htmlFor="flexCheckDefault6">
+                                    Upcoming
+                                </label>
+                                </div>
+                                </div>
                             </div>
                         </div>
                         <div className="col-md-9">
-                            {searchItems.length > 0 ?
+                            {displayItems.length > 0 ?
                             <div className='row row-cols-1 row-cols-md-3 g-4'>
-                            {searchItems.length === 0 ?
+                            {displayItems.length === 0 ?
                                 <div className='text-center mt-5'>
                                     <p>No Result Found</p>
                                 </div>
                             :
                                 <>
                                     {
-                                        searchItems.map(launch =>
+                                        displayItems.map(launch =>
                                             <SingleSpaceLaunch
                                             key={launch.mission_name} 
                                             launch={launch}
@@ -89,8 +220,21 @@ const SpaceLaunch = () => {
                         </div>
                     </div>
                 </div>
-
+                <Modal
+                size="sm"
+                show={smShow}
+                onHide={() => setSmShow(false)}
+                aria-labelledby="example-modal-sizes-title-sm"
+            >
+                <Modal.Header closeButton>
+                <Modal.Title id="example-modal-sizes-title-sm">
+                    Oppps!
+                </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>No Launches history found.</Modal.Body>
+            </Modal>            
         </div>
+        
     );
 };
 
