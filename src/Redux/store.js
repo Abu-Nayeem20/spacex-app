f496256196d41cc528dbf54ec192d74d464eb518
Abu-Nayeem20 <abu64673@gmail.com>
2022-05-10 23:02:01 +0600

    update

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
-import launchesReducer from './Slices/spaceSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import launchesReducer from './Slices/spaceSlice';
 
 export const store = configureStore({
     reducer: {
-      launches: launchesReducer,
+        launches: launchesReducer,
     },
-  })
+});
